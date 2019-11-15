e9d7e6b2b12945d3abc9088d5331721582a6d552
Sabry-63 <52372174+Sabry-63@users.noreply.github.com>
2019-11-15 08:29:32 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,30 +1,76 @@
 /*global $*/
-$('document').ready(function () {
-    'use strict';
+$("document").ready(function() {
+    "use strict";
     // Toggel User Form
-    $('.toggle-user-form').click(function () {
+    $(".toggle-user-form").click(function() {
         event.stopPropagation();
-        $('.user-form').fadeToggle();
+        $(".user-form").fadeToggle();
     });
-    
+
     // Pop up cart
-    $(".cart-popup").click(function (e) {
+    $(".cart-popup").click(function(e) {
         e.stopPropagation();
-        $('.pernt-popup').fadeToggle();
+        $(".pernt-popup").fadeToggle();
     });
-    $('.close-popup-cart').click(function () {
-        $('.pernt-popup').fadeOut();
+    $(".close-popup-cart").click(function() {
+        $(".pernt-popup").fadeOut();
     });
-    $(document).keydown(function (e) {
+    $(document).keydown(function(e) {
         if (e.keyCode === 27) {
-            $('.pernt-popup').fadeOut();
+            $(".pernt-popup").fadeOut();
         }
     });
-   /* ------ 
-   
-   // Categories Menu
-    $('.categories-menu').click(function () {
-        event.stopPropagation();
-        $('.drobdwon').fadeToggle();
-    }); */
+    // Pricing Slider
+    $(".pric-menu").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    //Testmonial said-testmonial
+    $('.said-testmonial').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        arrows: true,
+        prevArrow: '<i class="slick_left fas fa-angle-right"></i>',
+        nextArrow: '<i class="slick_right fas fa-angle-left"></i>',
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
+
 });
